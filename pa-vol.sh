91df58663234f7293fd207a12a933e663c61ac16
Tomasz Górecki <tomasz.gorecki@protonmail.com>
2019-03-21 07:55:20 +0100

    Adjusted the code to Shellcheck's advices

--- pa-vol.sh
+++ pa-vol.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
- 
+
 # CHANGES, RE-VIEWS/WRITES, IMPROVEMENTS:
-# mhasko: functions; VOL-steps; 
+# mhasko: functions; VOL-steps;
 # gashapon: autodectect default sink
 # konrad: more reliable way to autodetect default sink
- 
+
 # get default sink name
 SINK_NAME=$(pacmd dump | perl -a -n -e 'print $F[1] if /set-default-sink/')
 # try this line instead of the one above if you got problems with the detection of your default sink.
@@ -13,41 +13,43 @@ SOURCE_NAME=$(pacmd dump | perl -a -n -e 'print $F[1] if /set-default-source/')
 
 # set max allowed volume; 0x10000 = 100%
 VOL_MAX="0x10000"
- 
+
 STEPS="16" # 2^n
 VOL_STEP=$((VOL_MAX / STEPS))
- 
-VOL_NOW=`pacmd dump | grep -P "^set-sink-volume $SINK_NAME\s+" | perl -p -n -e 's/.+\s(.x.+)$/$1/'`
-MUTE_STATE=`pacmd dump | grep -P "^set-sink-mute $SINK_NAME\s+" | perl -p -n -e 's/.+\s(yes|no)$/$1/'`
-MIC_MUTE_STATE=`pacmd dump | grep -P "^set-source-mute $SINK_NAME\s+" | perl -p -n -e 's/.+\s(yes|no)$/$1/'`
- 
+
+VOL_NOW=$(pacmd dump | grep -P "^set-sink-volume $SINK_NAME\s+" | perl -p -n -e 's/.+\s(.x.+)$/$1/')
+MUTE_STATE=$(pacmd dump | grep -P "^set-sink-mute $SINK_NAME\s+" | perl -p -n -e 's/.+\s(yes|no)$/$1/')
+
+# Unsed as of now...
+# MIC_MUTE_STATE=`pacmd dump | grep -P "^set-source-mute $SINK_NAME\s+" | perl -p -n -e 's/.+\s(yes|no)$/$1/'`
+
 function plus() {
         VOL_NEW=$((VOL_NOW + VOL_STEP))
         if [ $VOL_NEW -gt $((VOL_MAX)) ]; then
                 VOL_NEW=$((VOL_MAX))
         fi
-        pactl set-sink-volume $SINK_NAME `printf "0x%X" $VOL_NEW`
+        pactl set-sink-volume "$SINK_NAME" "$(printf "0x%X" $VOL_NEW)"
 }
- 
+
 function minus() {
         VOL_NEW=$((VOL_NOW - VOL_STEP))
-        if [ $(($VOL_NEW)) -lt $((0x00000)) ]; then
+        if [ "$VOL_NEW" -lt $((0x00000)) ]; then
                 VOL_NEW=$((0x00000))
         fi
-        pactl set-sink-volume $SINK_NAME `printf "0x%X" $VOL_NEW`
+        pactl set-sink-volume "$SINK_NAME" "$(printf "0x%X" $VOL_NEW)"
 }
- 
+
 function mute() {
-        pactl set-sink-mute $SINK_NAME toggle
+        pactl set-sink-mute "$SINK_NAME" toggle
 }
 
 function micmute() {
-        pactl set-source-mute $SOURCE_NAME toggle
+        pactl set-source-mute "$SOURCE_NAME" toggle
 }
 
 function get() {
         BAR=""
-        if [ $MUTE_STATE = yes ]; then
+        if [ "$MUTE_STATE" = "yes" ]; then
                 BAR="mute"
                 ITERATOR=$((STEPS / 2 - 2))
                 while [ $ITERATOR -gt 0 ]; do
@@ -69,7 +71,7 @@ function get() {
         fi
         echo "$BAR"
 }
- 
+
 case "$1" in
         plus)
                 plus
